45f47808c479dfee7d11723e02117d1bd0076a95
Gabrielle Ecanow <gecanow@mit.edu>
2021-04-24 12:57:54 -0400

    WIP function library trie

--- function-library.scm
+++ function-library.scm
@@ -2,14 +2,47 @@
 Function Library
 
 ---------------------------------------------------------
-Project: Program Refactoring
+Project: 6.945 | Program Refactoring
 Date: 24 April 2021
 Authors: Gabrielle Ecanow, Marlena Gomez, Katherine Liew
 ---------------------------------------------------------
 |#
 
 
-(load "sdf/manager/load")
+(load "sdf/manager/load")             ; not sure if needed...
+(manage 'new 'generic-procedures)     ; loads common/trie.scm
+                                      ; maybe: just (load "sdf/common") ?
 
 (define fnc-library (make-trie))
+
+;;;; User interface
+
+(define (add-to-library name proc)) ; TODO
+
+;;;; Internal (helper) procedures  
+
+;;; Converts a (list) proc to a path of predicates suitable for the library trie
+(define (proc->path proc)
+  (if (null? proc)
+      '()
+      (let ((first-elem (car proc))
+	    (rest (cdr proc)))
+	(let ((elem-test (lambda (x) (eq? `,x first-elem))))
+	  (append `(,elem-test) (proc->path (cdr proc)))))))
+
+; ... testing ...
+
+(define test-proc '(+ 1 2 3))
+
+(define proc->path:test (proc->path test-proc))
+
+(define name (intern-path-trie fnc-library proc->path:test))
+
+(set-path-value! fnc-library proc->path:test 'name)
+
+;;; demonstration: get-a-value given a (list) proc should return the (procedure) name
+(get-a-value fnc-library '(+ 1 2 3)) 
+; -> name
+
+
 
