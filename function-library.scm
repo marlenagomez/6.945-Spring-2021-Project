051f1a2a26bc3b37d4141ace38a509b2e98f03c3
Gabrielle Ecanow <gecanow@mit.edu>
2021-04-24 12:11:02 -0400

    added SDF and initial library scm file

---
+++ function-library.scm
@@ -0,0 +1,15 @@
+#|
+Function Library
+
+---------------------------------------------------------
+Project: Program Refactoring
+Date: 24 April 2021
+Authors: Gabrielle Ecanow, Marlena Gomez, Katherine Liew
+---------------------------------------------------------
+|#
+
+
+(load "sdf/manager/load")
+
+(define fnc-library (make-trie))
+
