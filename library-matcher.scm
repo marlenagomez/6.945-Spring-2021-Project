c0b310d5e9d6e6d09f1c94501e90765597bcfcc3
Gabrielle Ecanow <gecanow@mit.edu>
2021-05-06 17:09:37 -0400

    transferred replacement code here

--- library-matcher.scm
+++ library-matcher.scm
@@ -36,11 +36,32 @@ Authors: Gabrielle Ecanow, Marlena Gomez, Katherine Liew
 		      (check-all (cdr chunks)))))))
   (check-all (chunkify scheme-code)))
 
+(define (library-compress scheme-code)
+  
+ ;(define replaced-record (make-alist-store eq?)) ; TODO: store record of what was replaced
+
+  (define (walk code)
+    (if (null? code)
+	'()
+	(map (lambda (elem)
+	       (if (list? elem) 
+		   (let ((search-result (find-in-library elem)))
+		     (if (null? search-result)
+			 (walk elem)
+			 (car search-result)))
+		   elem))
+	     code)))
+  (car (walk `(,scheme-code))))
+
+
+;------------------------------------------------------------------------
 ; ... testing ...
 
 (add-to-library 'expr-10 '(* x 3))
 (match-in-library '(* x 3))       ; -> (((* x 3) (expr-10)))
 (pp expr-10)                      ; -> (lambda () (* x 3))
+(library-compress '(* x 3))       ; -> (expr-10)
+(library-compress '(+ (* x 3) (* x 3) (* x 3) (* x 3))) ; -> (+ (expr-10) (expr-10) (expr-10) (expr-10))
 
 (add-to-library '1+2+3 '(+ 1 2 3))
 (match-in-library '(+ 1 2 3))                     ; -> (((+ 1 2 3) (|1+2+3|)))
@@ -48,26 +69,48 @@ Authors: Gabrielle Ecanow, Marlena Gomez, Katherine Liew
 
 (1+2+3) ; -> 6
 
+(mod10 2) ; -> Unbound variable: mod10
 (add-to-library 'mod10 `(modulo (? x ,symbol?) 10))
 (match-in-library '(define (x y z)
+		     (+ (modulo x 10) (modulo y 10) (modulo z 10))))   ; -> (((modulo x 10) (mod10 x)) ((modulo y 10) (mod10 y)) ((modulo z 10) (mod10 z)))
+(library-compress '(define (x y z)
 		     (+ (modulo x 10) (modulo y 10) (modulo z 10))))
-; -> (((modulo x 10) (mod10 x)) ((modulo y 10) (mod10 y)) ((modulo z 10) (mod10 z)))
+; -> (define (x y z) (+ (mod10 x) (mod10 y) (mod10 z)))
 
 (mod10 2) ; ->  2
 
-(add-to-library 'add-constant-map `(map (lambda (l) (+ l (? x ,number?))) (? y ,list?)))
+(add-to-library 'add-c-map `(map (lambda (l) (+ l (? x ,number?))) (? y ,list?)))
 (match-in-library '(define (a b c d e f)
 		     (append (map (lambda (l) (+ l 4)) (a b))
 			     (map (lambda (l) (+ l 5)) (c d))
 			     (map (lambda (l) (+ l 6)) (e f)))))
 #|
-(((map (lambda (l) (+ l 4)) (a b)) (add-constant-map 4 (a b))) 
- ((map (lambda (l) (+ l 5)) (c d)) (add-constant-map 5 (c d))) 
- ((map (lambda (l) (+ l 6)) (e f)) (add-constant-map 6 (e f))))
+(((map (lambda (l) (+ l 4)) (a b)) (add-c-map 4 (a b))) 
+ ((map (lambda (l) (+ l 5)) (c d)) (add-c-map 5 (c d))) 
+ ((map (lambda (l) (+ l 6)) (e f)) (add-c-map 6 (e f))))
 |#
 
-(add-constant-map 3 '(1 2 3)) ; -> (4 5 6)  
+(add-constant-map 3 '(1 2 3)) ; -> (4 5 6)
 
+(library-compress '(define (a b c d e f)
+		     (append (map (lambda (l) (+ l 4)) (a b c))
+			     (map (lambda (l) (+ l 5)) (c d))
+			     (map (lambda (l) (+ l 6)) (e f)))))
+#|
+(define (a b c d e f) 
+  (append (add-c-map 4 (a b c)) 
+	  (add-c-map 5 (c d)) 
+	  (add-c-map 6 (e f))))
+|#
+
+(library-compress '(define (a b c d e f)
+		     (append (map (lambda (l) (+ l 4)) (a b c))
+			     (map (lambda (m) (modulo m 10)) (d e f)))))
+#|
+(define (a b c d e f) 
+  (append (add-c-map 4 (a b c)) 
+	  (map (lambda (m) (mod10 m)) (d e f))))
+|#
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 #|
